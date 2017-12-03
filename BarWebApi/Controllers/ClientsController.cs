5b6fa73e65465cef54e6f77065c9d60cde817eb6
mpina <mpina83@gmail.com>
2017-12-03 19:20:43 +0000

    initial commit

---
+++ BarWebApi/Controllers/ClientsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClientsController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public ClientResults Get()
+        {
+            ClientResults clResult  = new ClientResults()
+            {
+                results = new string[] {"Joao", "Filipe","José"}
+            };
+            return clResult;
+        }
+    }
+    
+    public class ClientResults
+    {
+        public string[] results { get; set; }
+    }
+}
+
